a92eb260fa6c6587b7a0706bc2b3751c9851448f
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-05-02 01:22:57 +0800

    Add train_with_experiment func, in which currently data required for experiment 1 & 2

--- weight_reverse_model_interface.py
+++ weight_reverse_model_interface.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import numpy as np
+import random
 
 class WeightReverseModelInterface():
     
@@ -23,14 +24,16 @@ class WeightReverseModelInterface():
         self.batch_size = 1
         self.num_of_print_interval = 1
         self.input_size = 100000
+        self.num_of_weights_per_model = None
 
-    def set_hyperparameters(self, num_of_epochs=1, batch_size=1, num_of_print_interval=100, input_size=784):
+    def set_hyperparameters(self, num_of_epochs=1, num_of_print_interval=1, input_size=100000):
         self.num_of_epochs = num_of_epochs
-        self.batch_size = batch_size 
         self.num_of_print_interval = num_of_print_interval
         self.input_size = input_size
 
-    def set_dataset_loader(self, weights_dataset, outputs_dataset, predictions_dataset):
+    def set_dataset_loader(self, weights_dataset, outputs_dataset, predictions_dataset, batch_size):
+        self.num_of_weights_per_model = weights_dataset.shape[1]
+        self.batch_size = batch_size
         self.weights_loader = torch.utils.data.DataLoader(dataset=weights_dataset, batch_size=self.batch_size)
         self.outputs_loader = torch.utils.data.DataLoader(dataset=outputs_dataset, batch_size=self.batch_size)
         self.predictions_loader = torch.utils.data.DataLoader(dataset=predictions_dataset, batch_size=self.batch_size) 
@@ -47,9 +50,9 @@ class WeightReverseModelInterface():
         self.optimizer = optimizer
 
     def train(self):
-        total_step = self.num_of_train_samples
+        total_step = self.num_of_train_samples/self.batch_size
         for epoch in range(self.num_of_epochs):
-            for i, (weights, outputs, _) in enumerate(zip(self.weights_loader, self.outputs_loader, self.predictions_loader)):
+            for i, (weights, outputs, predictions) in enumerate(zip(self.weights_loader, self.outputs_loader, self.predictions_loader)):
                 
                 # Move tensors to the configured device
                 outputs = outputs.reshape(-1, self.input_size).to(self.device)
@@ -57,7 +60,8 @@ class WeightReverseModelInterface():
 
                 # Forwarding 
                 predicted_weights = self.model.forward(outputs)
-                loss = self.loss_func.forward(predicted_weights, weights, predicted_weights, weights)
+                # loss = self.loss_func.forward(predicted_weights, weights, weights, predictions)
+                loss, _, _ = self.loss_func.forward(predicted_weights, weights, predicted_weights, predictions)
 
                 # Optimization (back-propogation)
                 self.optimizer.zero_grad()
@@ -65,15 +69,70 @@ class WeightReverseModelInterface():
                 self.optimizer.step()
 
                 if (i+1) % self.num_of_print_interval == 0:
-                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_of_epochs, i+1, total_step/self.batch_size, loss.item()))
-                    
+                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_of_epochs, i+1, total_step, loss.item()))
                     '''
-                    1. loss stucks, maybe should use batch
-                    2. maybe should use VAE, which is suitable for abundant inputs and outputs training
-                    3. maybe separate weights into several pieces at train them individual and combine them to ensure the final prediction accurancy
-                    4. how to compute l2 
+                    1. maybe should use VAE, which is suitable for abundant inputs and outputs training
+                    2. maybe separate weights into several pieces at train them individual and combine them to ensure the final prediction accurancy
                     '''
-                    print(predicted_weights[0][25000], weights[0][25000])
+                    # print(predicted_weights[0][25000], weights[0][25000])
+
+    def train_with_experiment(self, experiment_number):
+        total_step = self.num_of_train_samples/self.batch_size
+        combined_losses, l1_losses, l2_losses = [], [], []
+
+        # Randomly select weight values for experiment 2
+        selected_neuron_indexes = []
+        data_for_experiment_2 = []
+        for _ in range(10):
+            selected_neuron_indexes.append((random.randint(0, self.batch_size-1), random.randint(0, self.num_of_weights_per_model-1)))
+            data_for_experiment_2.append([])
+        data_for_experiment_2.append([])
+
+        for epoch in range(self.num_of_epochs):
+            for i, (weights, outputs, predictions) in enumerate(zip(self.weights_loader, self.outputs_loader, self.predictions_loader)):
+                
+                # Move tensors to the configured device
+                outputs = outputs.reshape(-1, self.input_size).to(self.device)
+                weights = weights.to(self.device)
+
+                # Forwarding 
+                predicted_weights = self.model.forward(outputs)
+                loss, l1, l2 = self.loss_func.forward(predicted_weights, weights, predicted_weights, predictions)
+                
+                # collect data for experiment 1
+                combined_losses.append(loss.item())
+                l1_losses.append(l1.item())
+                l2_losses.append(l2.item())
+
+                # Optimization (back-propogation)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                if (i+1) % self.num_of_print_interval == 0:
+                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_of_epochs, i+1, total_step, loss.item()))
+                    cum_percentage_err = 0
+                    for i, (model_idx, weight_idx) in enumerate(selected_neuron_indexes):
+                        tar_val = weights[model_idx][weight_idx].item()
+                        pre_val = predicted_weights[model_idx][weight_idx].item()
+                        abs_diff = abs(tar_val - pre_val)
+                        abs_tar_val = abs(tar_val)
+                        abs_percentage_err = abs_diff / abs_tar_val
+                        cum_percentage_err += abs_percentage_err
+                        data_for_experiment_2[i].append(abs_percentage_err)
+
+                    avg_percentage_err = cum_percentage_err / len(selected_neuron_indexes)
+                    data_for_experiment_2[len(data_for_experiment_2)-1].append(avg_percentage_err)
+
+        print('Data of experiment', experiment_number, 'is successfully computed and returned')
+
+        results = None
+        if experiment_number == 1:
+            results = (combined_losses, l1_losses, l2_losses)
+        elif experiment_number == 2:
+            results = data_for_experiment_2
+
+        return results
 
     def test(self, generation_index):
         '''
