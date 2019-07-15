e3feb7a59425e0e38649d49fe4f785be4aae334e
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-07-15 17:18:09 +0800

    confine Reverser, cutting all experiments currently

--- Reverser/experiment_interface.py
+++ Reverser/experiment_interface.py
@@ -13,51 +13,24 @@ from .customized_loss_func import CustomerizedLoss
 
 class ExperimentInterface():
 
-    def __init__(self):
-        # self.whitebox_generator = WhiteboxModelGenerator()
-        self.whitebox_extractor = WhiteboxModelExtractor()
-        # self.init_whitebox_generator()
+    def __init__(self, num_of_model_extracted, batch_size, num_of_epochs, num_of_print_interval):
+        self.num_of_model_extracted = num_of_model_extracted
+        self.batch_size = batch_size
+        self.num_of_epochs = num_of_epochs
+        self.num_of_print_interval = num_of_print_interval
 
+        self.whitebox_extractor = WhiteboxModelExtractor()
         self.weight_reverse_model_interface = WeightReverseModelInterface() 
         self.init_weight_reverse_model_interface()
 
-    def init_whitebox_generator(self):
-        # Set dataset loader, including train loader and test loader 
-        train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=torchvision.transforms.ToTensor(), download=True)
-        test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=torchvision.transforms.ToTensor(), download=True)
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100, shuffle=True)
-        test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=10000, shuffle=False)
-        self.whitebox_generator.set_dataset_loader(train_loader, test_loader)
+        self.num_of_model_extracted = num_of_model_extracted
+        self.set_weightmodel_train_dataset(
+            self.whitebox_extractor.extract_whitebox_model_weights(self.num_of_model_extracted), 
+            self.whitebox_extractor.extract_whitebox_model_outputs(self.num_of_model_extracted),
+            self.whitebox_extractor.extract_whitebox_model_predictions(self.num_of_model_extracted),
+            self.batch_size)
 
-        '''
-        Set white-box architecture and its hyper parameters 
-        ''' 
-        class NeuralNet(nn.Module):
-            def __init__(self, input_size, hidden_size, output_size):
-                super().__init__()
-                self.layer1 = nn.Linear(input_size, hidden_size)
-                self.layer2 = nn.Linear(hidden_size, output_size)
-                self.relu = nn.ReLU()
-
-            def forward(self, x):
-                output = self.layer1(x)
-                output = self.relu(output)
-                output = self.layer2(output)
-                return output 
-
-        input_size, hidden_size, output_size = 784, 64, 10
-        model = NeuralNet(input_size, hidden_size, output_size)
-        self.whitebox_generator.set_model(model)
-
-        # Set loss function of white-box model         
-        self.whitebox_generator.set_loss_func(nn.CrossEntropyLoss())
-
-        # Set optimizer of white-box model 
-        learning_rate = 0.001
-        self.whitebox_generator.set_optimizer(torch.optim.Adam(model.parameters(), lr=learning_rate))
-
-        # Set hyperparameters for the generation (training) proces
-        self.whitebox_generator.set_generation_hyperparameters(num_of_epochs=1, num_of_print_interval=100, input_size=784)
+        self.set_weightmodel_hyperparameters(num_of_epochs=self.num_of_epochs, num_of_print_interval=self.num_of_print_interval)
 
     def init_weight_reverse_model_interface(self):
         '''
@@ -103,18 +76,6 @@ class ExperimentInterface():
     def train_weightmodel(self):
         self.weight_reverse_model_interface.train()
 
-    def train_weightmodel_experiment_1(self):
-        # it should returns lists of epochs_idxs, combined_losses, l1_losses, l2_losses
-        return self.weight_reverse_model_interface.train_with_experiment(1)
-
-    def generate_whitebox_model(self, num_of_model_generated):
-        self.whitebox_generator.generate(num_of_model_generated)
-
-    def extract_whitebox_model_weights(self, num_of_model_extracted):
-        return self.whitebox_extractor.extract_whitebox_model_weights(num_of_model_extracted)
-
-    def extract_whitebox_model_outputs(self, num_of_model_extracted):
-        return self.whitebox_extractor.extract_whitebox_model_outputs(num_of_model_extracted)
-
-    def extract_whitebox_model_predictions(self, num_of_model_extracted):
-        return self.whitebox_extractor.extract_whitebox_model_predictions(num_of_model_extracted)
+    # PENDING
+    def test_weightmodel(self):
+        pass
--- Reverser/weight_reverse_model_interface.py
+++ Reverser/weight_reverse_model_interface.py
@@ -78,64 +78,6 @@ class WeightReverseModelInterface():
                     '''
                     print(predicted_weights[0][25000], weights[0][25000])
 
-    def train_with_experiment(self, experiment_number):
-        total_step = self.num_of_train_samples/self.batch_size
-        combined_losses, l1_losses, l2_losses = [], [], []
-
-        # Randomly select weight values for experiment 2
-        selected_neuron_indexes = []
-        data_for_experiment_2 = []
-        for _ in range(10):
-            selected_neuron_indexes.append((random.randint(0, self.batch_size-1), random.randint(0, self.num_of_weights_per_model-1)))
-            data_for_experiment_2.append([])
-        data_for_experiment_2.append([])
-
-        for epoch in range(self.num_of_epochs):
-            for i, (weights, outputs, predictions) in enumerate(zip(self.weights_loader, self.outputs_loader, self.predictions_loader)):
-                
-                # Move tensors to the configured device
-                outputs = outputs.reshape(-1, self.input_size).to(self.device)
-                weights = weights.to(self.device)
-
-                # Forwarding 
-                predicted_weights = self.model.forward(outputs)
-                loss, l1, l2 = self.loss_func.forward(predicted_weights, weights, predicted_weights, predictions)
-                
-                # collect data for experiment 1
-                combined_losses.append(loss.item())
-                l1_losses.append(l1.item())
-                l2_losses.append(l2.item())
-
-                # Optimization (back-propogation)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-
-                if (i+1) % self.num_of_print_interval == 0:
-                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, self.num_of_epochs, i+1, total_step, loss.item()))
-                    cum_percentage_err = 0
-                    for i, (model_idx, weight_idx) in enumerate(selected_neuron_indexes):
-                        tar_val = weights[model_idx][weight_idx].item()
-                        pre_val = predicted_weights[model_idx][weight_idx].item()
-                        abs_diff = abs(tar_val - pre_val)
-                        abs_tar_val = abs(tar_val)
-                        abs_percentage_err = abs_diff / abs_tar_val
-                        cum_percentage_err += abs_percentage_err
-                        data_for_experiment_2[i].append(abs_percentage_err)
-
-                    avg_percentage_err = cum_percentage_err / len(selected_neuron_indexes)
-                    data_for_experiment_2[len(data_for_experiment_2)-1].append(avg_percentage_err)
-
-        print('Data of experiment', experiment_number, 'is successfully computed and returned')
-
-        results = None
-        if experiment_number == 1:
-            results = (combined_losses, l1_losses, l2_losses)
-        elif experiment_number == 2:
-            results = data_for_experiment_2
-
-        return results
-
     def test(self, generation_index):
         '''
         1. how to test, using the original loss functions, absolutate percentage error?
@@ -158,15 +100,5 @@ class WeightReverseModelInterface():
                 correct += (predictions == labels).sum().item()
                 
             print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
-
-    def save_model(self, str):
-        torch.save(self.model.state_dict(), str)
-
-    def save_query_outputs(self, query_outputs, str):
-        torch.save(query_outputs, str)
-
-    def save_query_predictions(self, query_predictions, str):
-        torch.save(query_predictions, str)
-        
 
     
